be1088e57d2cff3fb8b89590ff6d733f2a8f54ed
Aporoid <Aporoid@hotmail.com>
2019-02-10 14:04:26 -0600

    Second level half-completed
    Not enough time for now

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+
+	public GameObject playerLight;
+
+	private void Update()
+	{
+		LightOn();
+	}
+
+	void LightOn()
+	{
+		if (Input.GetKey(KeyCode.F))
+		{
+			playerLight.SetActive(true);
+		}
+		else
+		{
+			playerLight.SetActive(false);
+		}
+	}
+}
