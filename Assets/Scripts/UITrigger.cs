a6f6fc4052cdb468622f71e3a80d4bc4644a40b9
Aporoid <Aporoid@hotmail.com>
2019-02-09 19:29:53 -0600

    Basic design
    A few bricks, props, and triggers.

---
+++ Assets/Scripts/UITrigger.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UITrigger : MonoBehaviour
+{
+	public Text triggerText;
+	public Light triggerLight;
+	public GameObject Flashlight;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		triggerText.text = "Press E to interact";
+		triggerLight.enabled = true;
+		CheckCollection();
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		triggerText.text = "";
+		triggerLight.enabled = false;
+	}
+
+	private void CheckCollection()
+	{
+		if (Input.GetButtonDown("Interact"))
+		{
+			Flashlight.SetActive(false);
+		}
+	}
+
+}
