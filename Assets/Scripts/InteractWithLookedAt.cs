bbba1d3911acbc3f11d7052b0c44d27b741b2577
Aporoid <Aporoid@hotmail.com>
2019-02-24 14:16:54 -0600

    Tutorial #2 dones
    Interfaces, use of inheritence, etc

---
+++ Assets/Scripts/InteractWithLookedAt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Detects when the player presses the interact key whilst looking at an IInteractive
+/// </summary>
+
+public class InteractWithLookedAt : MonoBehaviour
+{
+	[SerializeField]
+	private DetectLookedAtInteractive detectLookedAtInteractive;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && detectLookedAtInteractive.LookedAtInteractive != null)
+		 {
+			Debug.Log("Player pressed the button.");
+			detectLookedAtInteractive.LookedAtInteractive.InteractWith();
+		 }
+    }
+}
---
+++ Assets/Scripts/Interfaces/IInteractive.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Interface for elements the player interacts with via the interact key.
+/// </summary>
+public interface IInteractive
+{
+	void InteractWith();
+}
