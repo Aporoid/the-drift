9f94cfc8fe10cb868b777060c4dc027b0d80364f
Aporoid <Aporoid@hotmail.com>
2019-03-29 14:19:30 -0500

    Locking and unlocking doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -5,9 +5,29 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class Door : InteractiveObject
 {
+	[Tooltip("Check this box to set door's lock status")]
+	[SerializeField]
+	private bool isLocked;
+
+	[Tooltip("Text that displays when the player looks at a locked door")]
+	[SerializeField]
+	private string lockedDisplayText = "Locked";
+
+	[Tooltip("This audio clip plays when the door is locked and tried to open.")]
+	[SerializeField]
+	private AudioClip lockedAudioClip;
+
+	[Tooltip("This audio clip plays when the door is interacted with and unlocked")]
+	[SerializeField]
+	private AudioClip openAudioClip;
+
+	//If this conidition is true, it will display the door is locked. If false, it will display the default text.
+	public override string DisplayText => isLocked ? lockedDisplayText : base.DisplayText;
+
 	private Animator animator;
 	private bool isOpen = false;
 	private int shouldOpenAnimParameter = Animator.StringToHash("shouldOpen");
+
 	/// <summary>
 	///  Using a constructor here to initialize displayText in the editor
 	/// </summary>
@@ -27,10 +47,18 @@ public class Door : InteractiveObject
 	{
 		if (!isOpen)
 		{
-			base.InteractWith();
-			animator.SetBool("shouldOpen", true);
-			displayText = string.Empty;
-			isOpen = true;
+			if (!isLocked)
+			{
+				audioSource.clip = openAudioClip;
+				animator.SetBool("shouldOpen", true);
+				displayText = string.Empty;
+				isOpen = true;
+			}
+			else // if the door is locked
+			{
+				audioSource.clip = lockedAudioClip;
+			}
+			base.InteractWith(); //this plays a SFX from InteractiveObject.cs
 		}
 	}
 
