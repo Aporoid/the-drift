ee7779eb9b8e79dfe8f92be3131845c163292112
Kyle Cook <Kyle.Cook@iam.local>
2019-04-04 18:25:11 -0500

    Starting work on rain issues

---
+++ Assets/Scripts/RainController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rainPrefab;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        rainPrefab.SetActive(false);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        rainPrefab.SetActive(true);
+    }
+
+}
