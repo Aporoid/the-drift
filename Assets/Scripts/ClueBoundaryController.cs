e3fa424835237663894c20ef48544ccba1e0bcfb
Aporoid <Aporoid@hotmail.com>
2019-04-20 12:46:14 -0500

    Adding updated maps, bounds
    Maps, bounding boxes, and the scripts that will control them.

---
+++ Assets/Scripts/ClueBoundaryController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This class is used to keep players in a set area until they find the next clue.
+/// </summary>
+public class ClueBoundaryController : MonoBehaviour
+{
+	public Text blockerText;
+	public Collider firstBlocker;
+}
---
+++ Assets/Scripts/MapController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// This class is used to indicate which part of the map the player is on in the Inventory screen.
+/// </summary>
+public class MapController : MonoBehaviour
+{
+	
+}
