7308155e2fed567167fe9f839cdf21892e7e270a
Aporoid <Aporoid@hotmail.com>
2019-04-28 17:07:23 -0500

    Adding Peter's Lines
    More or less done by this point.

--- Assets/Scripts/HintScript.cs
+++ Assets/Scripts/HintScript.cs
@@ -8,6 +8,9 @@ public class HintScript : MonoBehaviour
 	[SerializeField]
 	private Text peterResponses;
 
+	[SerializeField]
+	private Button backButton;
+
 	private new AudioSource audio;
 	//public AudioClip[] peterClips = new AudioClip[6];
 	public AudioClip peterHint1;
@@ -15,7 +18,8 @@ public class HintScript : MonoBehaviour
 	public AudioClip peterHint3;
 	public AudioClip peterHint4;
 	public AudioClip peterHint5;
-	public AudioClip peterHint6;
+
+	private float duration;
 
 	private void Start()
 	{
@@ -26,33 +30,58 @@ public class HintScript : MonoBehaviour
 	{
 		if (InventoryObject.clueNumber == 1)
 		{
+			backButton.enabled = false;
+			duration = peterHint1.length;
+			StartCoroutine(WaitForSound());
 			audio.PlayOneShot(peterHint1, 1);
-			peterResponses.text = "Uhhhh, sounds like it’s put between a couple of light posts. Try finding an area where there’s two right next to each other in a row, like buoys on a dock. That’s best I got, sorry Rick.";
+			peterResponses.text = "Uhhhh, sounds like it’s put between a couple of light posts out on the waterfront. Try finding an area where there’s two right next to each other in a row, near the water. That’s best I got, sorry Rick.";
+			StopCoroutine(WaitForSound());
+			backButton.enabled = true;
 		}
 		else if (InventoryObject.clueNumber == 2)
 		{
+			backButton.enabled = false;
+			duration = peterHint2.length;
+			StartCoroutine(WaitForSound());
 			audio.PlayOneShot(peterHint2, 1);
-			peterResponses.text = "I mean, my first instinct is to go where plants are, like a garden or something. But 'panes of tinted glass' makes me think it's like a greenhouse or something. Try there.";
+			peterResponses.text = "I mean, my first instinct is to go where plants are, like a garden or something. But 'panes of tinted glass' makes me think it's like a greenhouse. Try there.";
+			StopCoroutine(WaitForSound());
+			backButton.enabled = true;
 		}
 		else if (InventoryObject.clueNumber == 3)
 		{
+			backButton.enabled = false;
+			duration = peterHint3.length;
+			StartCoroutine(WaitForSound());
 			audio.PlayOneShot(peterHint3, 1);
 			peterResponses.text = "Hmm, if I’m to take a wild guess, this sounds like a funeral home or a graveyard to me. I’m not too much a fan of those places after my uncle Jacob…" + "\n" + " actually never mind, just do what you need to. Cemeteries or Funeral home";
+			StopCoroutine(WaitForSound());
+			backButton.enabled = true;
 		}
 		else if (InventoryObject.clueNumber == 4)
 		{
+			backButton.enabled = false;
+			duration = peterHint4.length;
+			StartCoroutine(WaitForSound());
 			audio.PlayOneShot(peterHint4, 1);
-			peterResponses.text = "Well obviously it makes it sound like you need a key, but most doors normally open in or out according to code. So only door I can think of that slides is a barn door. Try there maybe?";
+			peterResponses.text = "Well obviously it makes it sound like you need a key, and if I’m thinking about crowns, that’s probably a deed. Barnyard blues has me thinking that it’s around a farm or something, so the key can’t be too far. Try a farm.";
+			StopCoroutine(WaitForSound());
+			backButton.enabled = true;
 		}
 		else if (InventoryObject.clueNumber == 5)
 		{
+			backButton.enabled = false;
+			duration = peterHint5.length;
+			StartCoroutine(WaitForSound());
 			audio.PlayOneShot(peterHint5, 1);
-			peterResponses.text = "Now my aunt Maria is a much more religious person than I am, but I know what worship normally means. Try poking around the local church, maybe you'll find something there.";
+			peterResponses.text = "C’mon Rick, what else in a town has that much priority? Well, aside from its citizens, livestock, building codes, pie... right, rambling, I think it’s city hall. I’m getting a snack.";
+			StopCoroutine(WaitForSound());
+			backButton.enabled = true;
 		}
-		else if (InventoryObject.clueNumber == 6)
-		{
-			audio.PlayOneShot(peterHint6, 1);
-			peterResponses.text = "C'mon Rick, what else in town has that much priority? Well, aside from citizens, livestock, building codes, pie... Right, rambling, I think it's City Hall. I'm getting a snack.";
-		}
+	}
+
+	IEnumerator WaitForSound()
+	{
+		yield return new WaitForSeconds(duration);
 	}
 }
